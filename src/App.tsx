ee1963825d983cdb639d507b7bb4da7713272e85
binela <binela.skenderi@outlook.com>
2025-04-20 17:11:25 +0200

    importerat navbar och searchform i header rkivit text

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
 import './App.scss';
+import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom';
+import About from './pages/About/About';
+import Home from './pages/Home/Home';
+import BookList from './components/BookList/BookList';
+import BookDetails from './components/BookDetails/BookDetails';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/about',
+    element: <About />,
+  },
+  {
+    path: '/book',
+    element: <BookList />,
+    children: [
+      {
+        path: '/book/:id',
+        element: <BookDetails />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import SearchForm from '../SearchForm/SearchForm';
+import './Header.scss';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="holder">
+      <header className="header">
+        <Navbar />
+        <div className="header-content flex-c text-center text-white"></div>
+        <h2 className="header-title">find your book of choice</h2>
+        <br />
+        <p className="header-text fs-18 fw-3">
+          Explore our library and find your next favorite book
+        </p>
+        <SearchForm />
+      </header>
+    </div>
+  );
 };
 
 export default Header;
