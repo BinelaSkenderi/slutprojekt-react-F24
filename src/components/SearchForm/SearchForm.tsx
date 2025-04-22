c2bbaa99a65e08d27127cdba4fdcbc9bccdfaf86
binela <binela.skenderi@outlook.com>
2025-04-22 16:04:05 +0200

    fixat lite searchform

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { FaSearch } from 'react-icons/fa';
+import './SearchForm.scss';
 
 const SearchForm = () => {
-  return <div>SearchForm</div>;
+  return (
+    <div className="search-form">
+      <div className="container">
+        <div className="search-form-content">
+          <form className="search-form">
+            <div className="search-form-elem flex flex-sb bg-wh">
+              <input
+                type="text"
+                className="`form-cotrol"
+                placeholder="The Lost World ..."
+              />
+              <button type="submit" className="flex flex-c">
+                <FaSearch className="text-purple" size={32} />
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SearchForm;
