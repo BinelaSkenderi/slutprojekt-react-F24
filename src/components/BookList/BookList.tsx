cd7d4843d97798f5d1508e32c0a411b98b1b8234
binela <binela.skenderi@outlook.com>
2025-04-24 14:19:53 +0200

    Fixade BookList-komponenten: lade till hantering för cover-bilder

--- src/components/BookList/BookList.tsx
+++ src/components/BookList/BookList.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { useGlobalContext } from '../../context';
+import Book from '../BookList/Book';
+import Loading from '../Loader/Loader';
+import coverImg from '../../images/covernotfound.png';
+import './BookList.scss';
 
 const BookList = () => {
-  return <div>BookList</div>;
+  const { books, loading, resultTitle } = useGlobalContext();
+
+  if (loading) return <Loading />;
+
+  const booksWithCovers = books.map(book => ({
+    ...book,
+    id: book.id.replace('/works/', ''),
+    cover_img: book.cover_id
+      ? `https://covers.openlibrary.org/b/id/${book.cover_id}-L.jpg`
+      : coverImg,
+  }));
+
+  return (
+    <section className="booklist">
+      <div className="container">
+        <div className="section-title">
+          <h2>{resultTitle}</h2>
+        </div>
+        <div className="booklist-content grid">
+          {booksWithCovers.slice(0, 30).map((item, index) => (
+            <Book key={index} {...item} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default BookList;
