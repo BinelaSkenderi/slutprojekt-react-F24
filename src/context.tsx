78b02cb4fcd8f953a8c53c0649cdce045bc65482
binela <binela.skenderi@outlook.com>
2025-04-24 12:40:57 +0200

    Lagt till en enkel Loader-komponent

--- src/context.tsx
+++ src/context.tsx
@@ -16,6 +16,15 @@ type AppContextType = {
   setResultTitle: (title: string) => void;
 };
 
+type bookSingle = {
+  title: string;
+  key: string;
+  author_name: string[];
+  cover_i: number;
+  edition_count: number;
+  first_publish_year: number;
+};
+
 const AppContext = React.createContext<AppContextType | undefined>(undefined);
 
 const AppProvider = ({ children }: { children: ReactNode }) => {
@@ -30,7 +39,52 @@ const AppProvider = ({ children }: { children: ReactNode }) => {
       const response = await fetch(`${URL}${searchTerm}`);
       const data = await response.json();
       const { docs } = data;
+      console.log(docs);
       setBooks(docs);
+
+      if (docs) {
+        const newBooks = docs
+          .slice(0, 20)
+          .map(
+            (bookSingle: {
+              key: any;
+              author_name: any;
+              cover_i: any;
+              edition_count: any;
+              first_publish_year: any;
+              title: any;
+            }) => {
+              const {
+                key,
+                author_name,
+                cover_i,
+                edition_count,
+                first_publish_year,
+                title,
+              } = bookSingle;
+
+              return {
+                id: key,
+                author: author_name?.join(', '),
+                coverId: cover_i,
+                editionCount: edition_count,
+                firstPublishYear: first_publish_year,
+                title: title,
+              };
+            }
+          );
+
+        setBooks(newBooks); // om du använder useState
+        if (newBooks.length > 1) {
+          setResultTitle('Your Search Result');
+        } else {
+          setResultTitle('No Search Result Found!');
+        }
+      } else {
+        setBooks([]);
+        setResultTitle('No Search Result Found!');
+      }
+      setLoading(false);
     } catch (error) {
       console.log(error);
     }
